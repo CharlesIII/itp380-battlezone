c34d771d06f612142ad09ec46acd85da03623471
utopiafallen <utopiafallen@fe031ca6-8f9d-490d-92e4-ffe42a8c26f4>
2010-04-07 06:26:10 +0000

    Some more A* progress. Actor was modified to add some reminders in comments.

--- Battlezone/Engine/PathFinder.cs
+++ Battlezone/Engine/PathFinder.cs
@@ -21,11 +21,12 @@ namespace Battlezone.Engine
 
         StreamReader fileReader;
         Hashtable graph;    //key-value pair between "xyz" and Vertex objects
+        ArrayList navNodes; //list of all the Vector3 positions for navigation
 
         /// <summary>
         /// Constructs a new pathfinder object using the given input file for generating the graph with.
         /// </summary>
-        /// <param name="fileName">Text file containing the data to generate a graph</param>
+        /// <param name="fileName">File path to the input file.</param>
         public PathFinder(string fileName)
         {
             if (!File.Exists(fileName))
@@ -36,57 +37,105 @@ namespace Battlezone.Engine
             }
             else
             {
-                graph = new Hashtable(10);
-
-                //parse input file to build graph
-                fileReader = File.OpenText(fileName);
-                string input;
-                string[] tokens;
-                while ((input = fileReader.ReadLine()) != null)
+                try
                 {
-                    tokens = input.Split(' ');
-                    if (tokens.Length != EXPECTED_NUMBER_OF_TOKENS)
+                    LoadGraph(fileName);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception("Instantiation of PathFinder failed." , e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Parses the input file to generate a graph.
+        /// </summary>
+        /// <param name="fileName">File path to the input file.</param>
+        private void LoadGraph(string fileName)
+        {
+            graph = new Hashtable(10);
+            navNodes = new ArrayList(10);
+
+            //parse input file to build graph
+            fileReader = File.OpenText(fileName);
+            string input;
+            string[] tokens;
+            while ((input = fileReader.ReadLine()) != null)
+            {
+                tokens = input.Split(' ');
+                if (tokens.Length != EXPECTED_NUMBER_OF_TOKENS)
+                    throw new Exception("Input file is incorrectly formatted.");
+                else
+                {
+                    string pos1 = tokens[0];
+                    string pos2 = tokens[1];
+
+                    if (pos1.Length != EXPECTED_TOKEN_LENGTH || pos2.Length != EXPECTED_TOKEN_LENGTH)
                         throw new Exception("Input file is incorrectly formatted.");
                     else
                     {
-                        string pos1 = tokens[0];
-                        string pos2 = tokens[1];
+                        Vertex firstVertex;
+                        Vertex secondVertex;
 
-                        if (pos1.Length != EXPECTED_TOKEN_LENGTH || pos2.Length != EXPECTED_TOKEN_LENGTH)
-                            throw new Exception("Input file is incorrectly formatted.");
+                        if (graph.ContainsKey(pos1))
+                        {
+                            firstVertex = (Vertex)graph[pos1];
+                        }
                         else
                         {
-                            Vertex firstVertex;
-                            Vertex secondVertex;
-
-                            if (graph.ContainsKey(pos1))
-                            {
-                                firstVertex = (Vertex)graph[pos1];
-                            }
-                            else
-                            {
-                                firstVertex = new Vertex(float.Parse(pos1[0].ToString()), float.Parse(pos1[1].ToString()), float.Parse(pos1[2].ToString()));
-                                graph.Add(pos1, firstVertex);
-                            }
-
-                            if (graph.ContainsKey(pos2))
-                            {
-                                secondVertex = (Vertex)graph[pos2];
-                            }
-                            else
-                            {
-                                secondVertex = new Vertex(float.Parse(pos2[0].ToString()), float.Parse(pos2[1].ToString()), float.Parse(pos2[2].ToString()));
-                                graph.Add(pos2, secondVertex);
-                            }
-
-                            //establish link between both vertice
-                            firstVertex.connectedVertices.Add(secondVertex);
-                            secondVertex.connectedVertices.Add(firstVertex);
+                            firstVertex = new Vertex(float.Parse(pos1[0].ToString()), float.Parse(pos1[1].ToString()), float.Parse(pos1[2].ToString()));
+                            navNodes.Add(firstVertex.position);
+                            graph.Add(pos1, firstVertex);
                         }
+
+                        if (graph.ContainsKey(pos2))
+                        {
+                            secondVertex = (Vertex)graph[pos2];
+                        }
+                        else
+                        {
+                            secondVertex = new Vertex(float.Parse(pos2[0].ToString()), float.Parse(pos2[1].ToString()), float.Parse(pos2[2].ToString()));
+                            navNodes.Add(secondVertex.position);
+                            graph.Add(pos2, secondVertex);
+                        }
+
+                        //establish link between both vertice
+                        firstVertex.connectedVertices.Add(secondVertex);
+                        secondVertex.connectedVertices.Add(firstVertex);
                     }
                 }
-                Console.WriteLine("Graph successfully generated.");
             }
+            fileReader.Close();
+            Console.WriteLine("Graph successfully generated.");
+        }
+
+        /// <summary>
+        /// Gets all the navigation nodes in the level.
+        /// </summary>
+        /// <returns>An ArrayList containing</returns>
+        public ArrayList GetNavigationNodes()
+        {
+            return navNodes;
+        }
+
+        /// <summary>
+        /// Finds the shortest path between the two navigation nodes. It is assumed that the caller will provide 
+        /// only nodes that exist in the graph.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public ArrayList GetPath(Vector3 start, Vector3 end)
+        {
+            //list containing PotentialPaths that still need to be explored
+            ArrayList possibleSolutions = new ArrayList(15);
+
+            //generate keys from given Vectors for future use in graph look up
+            string startKey = start.X.ToString() + start.Y.ToString() + start.Z.ToString();
+            string endKey = end.X.ToString() + end.Y.ToString() + end.Z.ToString();
+
+            return null;
         }
 
         //compute heuristic as search progress
@@ -106,6 +155,21 @@ namespace Battlezone.Engine
             {
                 position = new Vector3(x, y, z);
                 connectedVertices = new ArrayList(10);
+            }
+        }
+
+        private class PotentialPath
+        {
+            public float currentCost;
+            public float estimatedCost;
+
+            public ArrayList path;
+
+            public PotentialPath()
+            {
+                currentCost = 0;
+                estimatedCost = 0;
+                path = new ArrayList(10);
             }
         }
     }
