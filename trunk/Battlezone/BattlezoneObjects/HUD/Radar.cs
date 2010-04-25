4a19c39005605210d5d39a8a11d54a72593f7440
charles.ciongoli.iii <charles.ciongoli.iii@fe031ca6-8f9d-490d-92e4-ffe42a8c26f4>
2010-04-25 20:08:16 +0000

    RADAR!!

---
+++ trunk/Battlezone/BattlezoneObjects/HUD/Radar.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Battlezone.BattlezoneObjects.HUD
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable & IDrawable.
+    /// </summary>
+    public class Radar : DrawableGameComponent
+    {
+        SpriteBatch mBatch;
+
+        private Texture2D PlayerDotImage;
+        private Texture2D EnemyDotImage;
+        private Texture2D RadarImage;
+
+        // Local coords of the radar image's center, used to offset image when being drawn
+        private Vector2 RadarImageCenter;
+
+        // Distance that the radar can "see"
+        private const float RadarRange = 2500.0f;
+        private const float RadarRangeSquared = RadarRange * RadarRange;
+
+        // Radius of radar circle on the screen
+        private const float RadarScreenRadius = 150.0f;
+
+        // This is the center position of the radar hud on the screen. 
+        static Vector2 RadarCenterPos = new Vector2(900.0f, 75.0f);
+
+        public Radar(Game game)
+            : base(game)
+        {
+            
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            mBatch = new SpriteBatch(Game.GraphicsDevice);
+
+            ContentManager aLoader = new ContentManager(Game.Services, "Content");
+
+            PlayerDotImage = aLoader.Load<Texture2D>("yellowDotSmall") as Texture2D;
+            EnemyDotImage = aLoader.Load<Texture2D>("redDotSmall") as Texture2D;
+            RadarImage = aLoader.Load<Texture2D>("blackDotLarge") as Texture2D;
+
+            RadarImageCenter = new Vector2(RadarImage.Width * 0.5f, RadarImage.Height * 0.5f);
+            System.Console.Out.WriteLine(RadarImage.Width);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            Vector3 playerFwd = GameplayScreen.Instance.getPlayer().GetWorldFacing();
+            Vector3 playerPos = GameplayScreen.Instance.getPlayer().GetWorldPosition();
+
+            mBatch.Begin();
+            // The last parameter of the color determines how transparent the radar circle will be
+            mBatch.Draw(RadarImage, RadarCenterPos, null, new Color(100, 100, 100, 150), 0.0f, RadarImageCenter, 1.0f, SpriteEffects.None, 0.0f);
+
+            float playerForwardRadians = (float)Math.Acos(Vector2.Dot(new Vector2(playerFwd.X, playerFwd.Z), new Vector2(0.0f, 1.0f)));
+            // If enemy is in range
+            foreach (AITank thisEnemy in GameplayScreen.Instance.Enemies)
+            {
+                Vector2 diffVect = new Vector2(thisEnemy.WorldPosition.X - playerPos.X, thisEnemy.WorldPosition.Z - playerPos.Z);
+                float distance = diffVect.LengthSquared();
+
+                // Check if enemy is within RadarRange
+                if (distance < RadarRangeSquared)
+                {
+                    // Scale the distance from world coords to radar coords
+                    diffVect *= RadarScreenRadius / RadarRange;
+
+                    // We rotate each point on the radar so that the player is always facing UP on the radar
+                    diffVect = Vector2.Transform(diffVect, Matrix.CreateRotationZ(playerForwardRadians));
+
+                    // Offset coords from radar's center
+                    diffVect += RadarCenterPos;
+
+                    // We scale each dot so that enemies that are at higher elevations have bigger dots, and enemies
+                    // at lower elevations have smaller dots.
+                    float scaleHeight = 1.0f + ((thisEnemy.WorldPosition.Y - playerPos.Y) / 200.0f);
+
+                    // Draw enemy dot on radar
+                   mBatch.Draw(EnemyDotImage, diffVect, null, Color.White, 0.0f, new Vector2(0.0f, 0.0f), scaleHeight, SpriteEffects.None, 0.0f);
+                }
+            }
+
+            // Draw player's dot last
+            mBatch.Draw(PlayerDotImage, RadarCenterPos, Color.White);
+
+            mBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
